bd7814570e94137369ead1ca9b374db789d4e5bf
kimulaco <kimulaco@gmail.com>
2022-10-12 09:17:52 +0900

    Change deploy command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-echo "gcloud run deploy trophy-comp --region us-central1 --source . --image us-central1-docker.pkg.dev/trophy-comp/cloud-run-source-deploy/trophy-comp --set-env-vars BUILD_MODE=production,CORS_ALLOW_ORIGIN=$CORS_ALLOW_ORIGIN,STEAM_API_KEY=$STEAM_API_KEY
+echo "gcloud run deploy $DEPLOY_PROJECT_NAME --region $DEPLOY_REGION --image $DEPLOY_IMAGE --set-env-vars BUILD_MODE=production,CORS_ALLOW_ORIGIN=$CORS_ALLOW_ORIGIN,STEAM_API_KEY=$STEAM_API_KEY
 "
 
-gcloud run deploy trophy-comp --region us-central1 --image us-central1-docker.pkg.dev/trophy-comp/cloud-run-source-deploy/trophy-comp --set-env-vars BUILD_MODE=production,CORS_ALLOW_ORIGIN=$CORS_ALLOW_ORIGIN,STEAM_API_KEY=$STEAM_API_KEY
+gcloud run deploy $DEPLOY_PROJECT_NAME --region $DEPLOY_REGION --image $DEPLOY_IMAGE --set-env-vars BUILD_MODE=production,CORS_ALLOW_ORIGIN=$CORS_ALLOW_ORIGIN,STEAM_API_KEY=$STEAM_API_KEY
