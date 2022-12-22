3d643f5979b0675cde441d57802e43e2f0cc4a54
kimulaco <kimulaco@gmail.com>
2022-12-22 09:35:01 +0900

    test: Add TestGetSteamUserTrophy_403

--- internal/api/getSteamUserTrophy/getSteamUserTrophy_test.go
+++ internal/api/getSteamUserTrophy/getSteamUserTrophy_test.go
@@ -71,7 +71,7 @@ func TestGetSteamUserTrophy_AppidNotFound(t *testing.T) {
 	}
 }
 
-func TestGetSteamUserTrophy_AppNotFound(t *testing.T) {
+func TestGetSteamUserTrophy_400(t *testing.T) {
 	t.Setenv("STEAM_API_KEY", "XXXXXXXX")
 	t.Setenv("STEAM_API_BASE_URL", "http://localhost:9999")
 
@@ -87,6 +87,43 @@ func TestGetSteamUserTrophy_AppNotFound(t *testing.T) {
 				Success: false,
 			},
 		},
+	})
+
+	rec, c := testdata.InitEcho("/api/steam/user/:steamid/trophy", "appid=1")
+	c.SetParamNames("steamid")
+	c.SetParamValues("1")
+
+	if assert.NoError(t, GetSteamUserTrophy(c)) {
+		assert.Equal(t, 200, rec.Code)
+
+		resBody, err := httputil.ReadBody[SuccessResponse](rec.Result())
+		assert.Equal(t, nil, err)
+
+		expected := SuccessResponse{
+			StatusCode: 200,
+			Trophies: []Trophy{
+				{
+					AppId:        1,
+					Success:      false,
+					GameName:     "",
+					Achievements: []steamworks.Achievement{},
+				},
+			},
+		}
+		assert.Equal(t, expected, resBody)
+	}
+}
+
+func TestGetSteamUserTrophy_403(t *testing.T) {
+	t.Setenv("STEAM_API_KEY", "XXXXXXXX")
+	t.Setenv("STEAM_API_BASE_URL", "http://localhost:9999")
+
+	defer gock.Off()
+	testdata.InitGock(testdata.GockConfig{
+		Url:      "http://localhost:9999",
+		Path:     steamworks.GetPlayerAchievementsPath,
+		StatusCode: 403,
+		Response: testdata.SteamworksResponse403,
 	})
 
 	rec, c := testdata.InitEcho("/api/steam/user/:steamid/trophy", "appid=1")
---
+++ testdata/steamworks.go
@@ -0,0 +1,14 @@
+package testdata
+
+var SteamworksResponse403 = `<html>
+
+<head>
+	<title>Forbidden</title>
+</head>
+
+<body>
+	<h1>Forbidden</h1>Access is denied. Retrying will not help. Please verify your
+	<pre>key=</pre> parameter.
+</body>
+
+</html>`
