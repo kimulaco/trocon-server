d8aa7dff4475af6c9a4409c2e3dbbe6c33ababc0
kimulaco <11986753+kimulaco@users.noreply.github.com>
2023-04-07 08:05:01 +0900

    feat: Use goroutine to GetSteamUserAPI (#11)

--- internal/api/getSteamUser/getSteamUser.go
+++ internal/api/getSteamUser/getSteamUser.go
@@ -27,25 +27,65 @@ func GetUser(c echo.Context) error {
 		return c.JSON(httputil.NewError400("STEAM_USER_STEAMID_NOT_FOUND", "steamid not found"))
 	}
 
-	player, err := s.GetPlayerSummary(steamid)
-	if err != nil {
-		log.Print("STEAM_USER_INTERNAL_ERROR: " + err.Error())
+	var playerCh UserChannel
+	var ownedGamesCh UserChannel
+	ch := make(chan UserChannel, 2)
+
+	go getPlayerSummary(s, steamid, ch,)
+	go getOwnedGames(s, steamid, ch,)
+
+	// TODO: Refactor channel receiving
+	userCh1, userCh2 := <- ch, <- ch
+
+	for _, c := range []UserChannel{userCh1, userCh2} {
+		if (len(c.player.SteamID) > 0) {
+			playerCh = c
+		} else {
+			ownedGamesCh = c
+		}
+	}
+
+	if playerCh.err != nil {
+		log.Print("STEAM_USER_INTERNAL_ERROR: " + playerCh.err.Error())
 		return c.JSON(httputil.NewError500("STEAM_USER_INTERNAL_ERROR", ""))
 	}
 
-	if player.IsEmpty() {
+	if playerCh.player.IsEmpty() {
 		return c.JSON(httputil.NewError404("STEAM_USER_NOT_FOUND", "user not found"))
 	}
 
-	ownedGames, err := s.GetOwnedGames(steamid)
-	if err != nil {
-		log.Print("STEAM_OWNED_GAME_INTERNAL_ERROR: " + err.Error())
+	if ownedGamesCh.err != nil {
+		log.Print("STEAM_OWNED_GAME_INTERNAL_ERROR: " + ownedGamesCh.err.Error())
 		return c.JSON(httputil.NewError500("STEAM_OWNED_GAME_INTERNAL_ERROR", ""))
 	}
 
 	return c.JSON(http.StatusOK, GetUserResponse{
 		StatusCode: http.StatusOK,
-		User:       player,
-		Games:      steamworks.MapOwnedGamesToGames(ownedGames),
+		User:       playerCh.player,
+		Games:      steamworks.MapOwnedGamesToGames(ownedGamesCh.ownedGames),
 	})
+}
+
+type  UserChannel struct {
+	player steamworks.Player
+	ownedGames []steamworks.OwnedGame
+	err error
+}
+
+func getPlayerSummary (s steamworks.Steamworks, steamid string, ch chan UserChannel) {
+	player, err := s.GetPlayerSummary(steamid)
+
+	ch <- UserChannel{
+		player: player,
+		err: err,
+	}
+}
+
+func getOwnedGames (s steamworks.Steamworks, steamid string, ch chan UserChannel) {
+	ownedGames, err := s.GetOwnedGames(steamid)
+
+	ch <- UserChannel{
+		ownedGames: ownedGames,
+		err: err,
+	}
 }
